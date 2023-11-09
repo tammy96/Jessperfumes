0d14588e1bc922aeaf1fd7b015deac2a7732aa47
ogiamien emmanuel tammy <ogiamien222@gmail.com>
2023-11-09 21:50:08 +0100

    fourth commit

--- src/About-Us/Gallery.js
+++ src/About-Us/Gallery.js
@@ -15,37 +15,37 @@ const Gallery = () => {
                 <h1 className="font-unna text-4xl text-navpurple opacity-60 md:text-6xl py-8 text-center">Gallery Product</h1>
             <div className="flex flex-row justify-center flex-wrap gap-2">
                 <div className="relative w-36 h-28 md:w-44 md:h-36 lg:w-96 lg:h-72  group">
-                    <img src={navimage5} alt="first image" className=" w-full h-full md:object-cover" />  
+                    <img src={navimage5} alt="first image" className=" w-full h-full object-cover md:object-cover" />  
                     <div className="w-full h-full absolute top-0 left-0 cursor-pointer bg-navpurple/50 
                     transition duration-300 ease-in-out overflow-hidden opacity-0 hover:opacity-100">
                 </div>
                 </div>
                 <div className="relative w-36 h-28 md:w-44 md:h-36 lg:w-96 lg:h-72  group">
-                    <img src={navimage2} alt="first image" className="w-full h-full md:object-cover" />  
+                    <img src={navimage2} alt="first image" className="w-full h-full object-cover md:object-cover" />  
                     <div className="w-full h-full absolute top-0 left-0 cursor-pointer bg-navpurple/50 
                     transition duration-300 ease-in-out overflow-hidden opacity-0 hover:opacity-100">
                 </div>
                 </div>
                 <div className="relative w-36 h-28 md:w-44 md:h-36 lg:w-96 lg:h-72  group">
-                    <img src={navimage3} alt="first image" className="w-full h-full md:object-cover" />  
+                    <img src={navimage3} alt="first image" className="w-full h-full object-cover md:object-cover" />  
                     <div className="w-full h-full absolute top-0 left-0 cursor-pointer bg-navpurple/50 
                     transition duration-300 ease-in-out overflow-hidden opacity-0 hover:opacity-100">
                 </div>
                 </div>
                 <div className="relative w-36 h-28 md:w-44 md:h-36 lg:w-96 lg:h-72  group">
-                    <img src={navimage4} alt="first image" className="w-full h-full md:object-cover" />  
+                    <img src={navimage4} alt="first image" className="w-full h-full object-cover md:object-cover" />  
                     <div className="w-full h-full absolute top-0 left-0 cursor-pointer bg-navpurple/50 
                     transition duration-300 ease-in-out overflow-hidden opacity-0 hover:opacity-100">
                 </div>
                 </div>
         <div className="relative w-36 h-28 md:w-44 md:h-36 lg:w-96 lg:h-72 group">
-            <img src={navimage} alt="first image" className="w-full h-full md:object-cover" />  
+            <img src={navimage} alt="first image" className="w-full h-full object-cover md:object-cover" />  
             <div className="w-full h-full absolute top-0 left-0 cursor-pointer bg-navpurple/50 
             transition duration-300 ease-in-out overflow-hidden opacity-0 hover:opacity-100">
           </div>
           </div>
         <div className="relative w-36 h-28 md:w-44 md:h-36 lg:w-96 lg:h-72 group">
-            <img src={navimage6} alt="first image" className="w-full h-full md:object-cover" />  
+            <img src={navimage6} alt="first image" className="w-full h-full object-cover md:object-cover" />  
             <div className="w-full h-full absolute top-0 left-0 cursor-pointer bg-navpurple/50 
             transition duration-300 ease-in-out overflow-hidden opacity-0 hover:opacity-100">
         </div>
